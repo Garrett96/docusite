e1e5089a5d7e82ff4e5619c6f9528151e0b06170
Garrett <46694517+Garrett96@users.noreply.github.com>
2025-03-19 23:48:25 -0400

    ws test

--- api/websocket-proxy.js
+++ api/websocket-proxy.js
@@ -1,36 +1,17 @@
 import { getConfig } from '@vercel/edge-config';
 
 export default async function handler(req) {
-  if (req.headers.get('Upgrade') !== 'websocket') {
-    return new Response('Upgrade required for WebSocket', { status: 400 });
-  }
-
   const connectionString = process.env.EDGE_CONFIG_CONNECTION_STRING;
 
   const config = await getConfig(connectionString);
 
-  const websocketHost = config.WEBSOCKETS_HOST || 'wss://docusite-git-live-edit-garrett96s-projects.vercel.app/api/websocket-proxy';
+  const websocketHost = config.WEBSOCKETS_HOST || 'wss://docusite-git-live-edit-garrett96s-projects.vercel.app';
   const websocketPort = config.WEBSOCKETS_PORT || '9999';
 
-  const websocketUrl = `${websocketHost}:${websocketPort}`;
-
-  console.log(`Proxying WebSocket to: ${websocketUrl}`);
-
-  const websocketConnection = await fetch(websocketUrl, {
-    method: 'CONNECT', 
-    headers: req.headers,
-  });
-
-  if (websocketConnection.ok) {
-    return new Response(null, {
-      status: 101, 
-      statusText: 'Switching Protocols',
-      headers: {
-        'Upgrade': 'websocket',
-        'Connection': 'Upgrade',
-      },
-    });
+  if (req.headers.get('Upgrade') === 'websocket') {
+    const websocketUrl = `${websocketHost}:${websocketPort}`;
+    return new Response(`Proxying WebSocket to: ${websocketUrl}`);
   } else {
-    return new Response('Failed to establish WebSocket connection', { status: 500 });
+    return new Response('Upgrade required for WebSocket', { status: 400 });
   }
 }
